d6fc0e4cd875dd08635f21be0ada46e9aaa03120
Yabiti <yabetseyohannes@gmail.com>
2023-08-09 14:52:20 +0300

    10-check_cycle.c,

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,12 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,3 +23,30 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
+
+carrie@ubuntu:~/0x00$ cat 10-linked_lists.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
