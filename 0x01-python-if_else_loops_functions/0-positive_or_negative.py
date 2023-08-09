9bebc6501fd429bc356703d0b11964e09369c4a8
Yabiti <yabetseyohannes@gmail.com>
2023-08-09 19:59:27 +0300

    positive or nrgative

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print ("the number is positive\n")
+if number == 0:
+    print ("the number is zero\n")
+if number < 0:
+    print ("the number is negative\n")
