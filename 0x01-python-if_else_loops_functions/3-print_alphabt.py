73638a487fde3ee8b7a473c376b08cad6964165b
Yabiti <yabetseyohannes@gmail.com>
2023-08-10 16:18:31 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for alpha in range(97, 123):
-    if alpha == 101 or alpha == 113:
+    if alpha == 101 or alpha == 123:
         alpha = alpha + 1
     else:
         print("{}".format(chr(alpha)), end='')
+    
