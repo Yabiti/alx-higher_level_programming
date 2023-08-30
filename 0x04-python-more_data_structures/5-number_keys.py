e39c07796e3e99c1bdcde9863eb88dbe86e656b7
Yabiti <yabetseyohannes@gmail.com>
2023-08-30 19:52:52 +0300

    keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 
-def print_sorted_dictionary(a_dictionary):
+def number_keys(a_dictionary):
     """
-    A function that prints a dictionary by ordered keys
+    A function that returns the number of keys in a dictionary.
     """
-    keys = list(a_dictionary.keys())
-    keys.sort()
-    for key in keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+    return len(a_dictionary)
