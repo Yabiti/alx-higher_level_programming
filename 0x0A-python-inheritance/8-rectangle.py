9252071822307bdae123321ad3411ed6ba1e2ef3
Yabiti <yabetseyohannes@gmail.com>
2023-09-11 16:56:03 +0300

    area

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Improve Geometry.
+"""Integer validator
 Author: Kanneh
 
 """
 
 
 class BaseGeometry:
-    """An empty class"""
+    """A Base Geometry class"""
 
     def area(self):
         """Raises an exception because...
@@ -14,3 +14,26 @@ class BaseGeometry:
 
         """
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the inputs: name and values
+
+        Args:
+            name(str) - input name as string
+            value(int): pararams validator
+
+        """
+        if type(value) not in [int]:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Instantiation of width and height"""
+        self.integer_validator(width, height)
+        self.__width = width
+        self.__height = height
