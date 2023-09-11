d4b486e2d0f3f5fbd62fd877fed0ebb138c002d4
Yabiti <yabetseyohannes@gmail.com>
2023-09-11 16:10:50 +0300

    class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -12,7 +12,6 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    for type(obj) == a_class:
+    if type(obj) == a_class:
         return True
     return False
-    
