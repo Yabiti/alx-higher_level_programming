cdcf3d491c1bc71aa80065670b7172eb43aa816e
Yabiti <yabetseyohannes@gmail.com>
2023-10-01 11:31:56 +0300

    self

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
-
-"""inherits from Rectangle (9-rectangle.py).
-(task based on 10-square.py
-
-"""
+# 11-square.py
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class reps of a rectangle"""
+    """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
