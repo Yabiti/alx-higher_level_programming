4363e0a013c852f45b6d363f9bd9d8cfbb92c2ad
Yabiti <yabetseyohannes@gmail.com>
2023-10-15 20:22:16 +0300

    llol

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,23 @@
 def main():
     print("this is main function")
     conditional_exec()
+    conditional_values()
+
 
 def conditional_exec():
-    a,b = 1, 3
+    a, b= 1, 3
     if a < b:
         print("a is less than b")
     elif a > b:
         print("a is greater than b")
     else:
         print("a is equal to b")
+
+
+def conditional_values():
+    a, b = 1, 2
+    statements = "less than" if a < b else"not less than"
+    print(statements)
 
 if __name__ == "__main__":
     main()
