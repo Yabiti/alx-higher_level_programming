e5ec0330fb94ca82a824d6c8527fe828a2e0d3e0
Yabiti <yabetseyohannes@gmail.com>
2023-10-15 19:34:41 +0300

    llok

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-# 0-lookup.py
-"""Defines an object attribute lookup function."""
+Class Human:
 
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+def __init__(self, kind = "good"):
+    self.kind = kind
+def what kind(self):
+retrun self.kind
+
+def main():
+    good Human = Human()
+    print(good Humnan.whatkind())
+    Bad Human = Human("Bad")
+    print(BadHumna.what kind())
+
+if__name__ == "__main__":
+    main()
