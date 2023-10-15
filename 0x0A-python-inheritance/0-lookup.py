699f1a6179a3d3d9687f86700cceeacb482817a3
Yabiti <yabetseyohannes@gmail.com>
2023-10-15 11:22:53 +0300

    look

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 0-lookup.py
-"""Defines an object attribute lookup function."""
 
+def main():
+    print("i am a general python syntax file")
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+if __name__ == "__main__":
+    main()
