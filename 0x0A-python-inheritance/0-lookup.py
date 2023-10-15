1c5770e804ec00b54ea931fd6813c2eb56a2327e
Yabiti <yabetseyohannes@gmail.com>
2023-10-15 19:41:06 +0300

    llo

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-Class Human:
+Class =  Human:
 
 
-def __init__(self, kind = "good"):
-    self.kind = kind
+def __init__(self, kind = "Good"):
+    def what kind = kind
+
 def what kind(self):
-retrun self.kind
 
-def main():
-    good Human = Human()
-    print(good Humnan.whatkind())
-    Bad Human = Human("Bad")
-    print(BadHumna.what kind())
-
-if__name__ == "__main__":
-    main()
+def kind():
+    return self.kind
