00313feb20799818a91d95a89d07ab52d9bae701
Yabiti <yabetseyohannes@gmail.com>
2023-10-16 19:19:51 +0300

    llol

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-
+import re
 def main():
-    loops(0)
-    loops()
-    loops(3)
-def loops(a = 4):
-    for i in range(a,6):
-        print(i, " ")
+    CompilerAndReplaceWord()
+
+def CompilerAndReplaceWord():
+    try:
+        files = open(",,/primary/file.txt")
+        pattern = re.Compiler('len|nevermore', re.IGNORECASE)
+        for line in files:
+            if re.Search(pattern, line):
+                print(pattern.Sub("#######", line), end = ' ')
+    except FileNotFoundError as e:
+        print("File Not Found:", e)
 
 if __name__ == "__main__":
     main()
-
