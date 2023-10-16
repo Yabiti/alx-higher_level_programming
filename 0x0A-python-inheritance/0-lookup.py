3fdb71c456ec70c642c13bd55b2f4c05556e3973
Yabiti <yabetseyohannes@gmail.com>
2023-10-16 19:41:31 +0300

    llol

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,28 +1,4 @@
-#!/usr/bin/python3
-import re
-def main():
-    FindWord()
-    DemarcationLine()
-    MatchWord()
-def FindWord():
-    try:
-        files = open(",,/primary/file.txt")
-        for line in files:
-            if re.Search('lenor/more', line):
-                print(line, end= ' ')
-    except FileNotFoundError as e:
-        print(" File NOt Found Error as", e)
-def MatchWord():
-    try:
-        files = open(",,/primary/file.txt")
-        for line in files:
-            match = re.search('len/nevermore', line)
-            if match:
-                print(MatchWord())
-    except FileNotFoundError as e:
-        print(" File Was NOt Found", e)
-def DemarcationLine():
-    print("***********")
-
-if __name__ == "__main__":
-    main()
+b = 1
+while b < 50:
+    print(b)
+    b = b + 1
