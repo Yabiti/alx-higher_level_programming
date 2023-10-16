7d6eb6d6b252a141355eba4900db870563bff15f
Yabiti <yabetseyohannes@gmail.com>
2023-10-16 19:39:34 +0300

    llol

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 import re
 def main():
-    CompilerAndReplaceWord()
-
-def CompilerAndReplaceWord():
+    FindWord()
+    DemarcationLine()
+    MatchWord()
+def FindWord():
     try:
         files = open(",,/primary/file.txt")
-        pattern = re.Compiler('len|nevermore', re.IGNORECASE)
         for line in files:
-            if re.Search(pattern, line):
-                print(pattern.Sub("#######", line), end = ' ')
+            if re.Search('lenor/more', line):
+                print(line, end= ' ')
     except FileNotFoundError as e:
-        print("File Not Found:", e)
+        print(" File NOt Found Error as", e)
+def MatchWord():
+    try:
+        files = open(",,/primary/file.txt")
+        for line in files:
+            match = re.search('len/nevermore', line)
+            if match:
+                print(MatchWord())
+    except FileNotFoundError as e:
+        print(" File Was NOt Found", e)
+def DemarcationLine():
+    print("***********")
 
 if __name__ == "__main__":
     main()
