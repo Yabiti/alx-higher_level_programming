6b8bbbba80b381049d169d4a0ac2309c05e0045e
Yabiti <yabetseyohannes@gmail.com>
2023-10-17 19:09:15 +0300

    groups

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- a script that delete the database hbtn_0c_0 in your MySQL server 
+-- because Batch 3 is the best!
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score
+ORDER BY score DESC;
