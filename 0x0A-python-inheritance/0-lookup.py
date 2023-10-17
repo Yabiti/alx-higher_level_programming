0eaf2315c572c9badd25d18fee25c79deadab25c
Yabiti <yabetseyohannes@gmail.com>
2023-10-17 10:32:49 +0300

    function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,8 +3,8 @@
 def main():
     PassingParameters(1,2,3)
 
-def PassingParameters(argument1, argument2, argumnet3):
-    print("here is our arguments")
+def PassingParameters(argument1, argument2, argument3):
+    print("here is our arguments:", argument1, argument2, argument3)
 
 if __name__ == "__main__":
     main()
