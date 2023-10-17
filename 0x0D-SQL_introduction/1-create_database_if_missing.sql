37c9739c4f3d31e8c5e67d84937425b0947f3fb0
Yabiti <yabetseyohannes@gmail.com>
2023-10-17 18:08:48 +0300

    create

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- Write a script that creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+-- a script that creates the database hbtn_0c_0 in your MySQL server 
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
