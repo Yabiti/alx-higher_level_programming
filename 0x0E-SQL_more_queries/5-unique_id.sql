e1132c39550ce725991fca9219da85c10762a445
Yabiti <yabetseyohannes@gmail.com>
2023-10-18 16:14:31 +0300

    id

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- a script that delete the database hbtn_0c_0 in your MySQL server 
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name varchar(256)
+);
