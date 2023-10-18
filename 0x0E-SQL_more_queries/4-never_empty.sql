fba3c8a7e61861923c8d8d2c85bbf309714e125e
Yabiti <yabetseyohannes@gmail.com>
2023-10-18 16:11:48 +0300

    default

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- a script that delete the database hbtn_0c_0 in your MySQL server 
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name varchar(256)
+);
