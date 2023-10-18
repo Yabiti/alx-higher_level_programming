a7bfea3e6cc3246b304b9f8ae33e7c39d3b8e66d
Yabiti <yabetseyohannes@gmail.com>
2023-10-18 16:23:22 +0300

    select

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,11 @@
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+
+-- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+-- The database name will be passed as an argument of the mysql command
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
