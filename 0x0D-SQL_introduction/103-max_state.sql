3bc33bb396596320f8fbb64835bf35b9a7f8fb1b
Yabiti <yabetseyohannes@gmail.com>
2023-10-19 19:21:47 +0300

    list

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 ALTER TABLE <table name>(
-ADD CONSTRAINT <constraint n> FOREIGN KEY <attribute list>
-REFERENCES <parent name> (<attribute list>);
+ADD CONSTRAINT <constraint n> FOREIGN KEY (<attribute list>)
+REFERENCES <parent table name> (<attribute list>);
 )
