4e427cebc53ab35d76c99415258c7c0e1ef858dd
Yabiti <yabetseyohannes@gmail.com>
2023-10-19 19:16:51 +0300

    list

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 ALTER TABLE <table name>(
-ADD CONSTRAINT <constraint> FORIEGN KEY <attribute list>
-REFERENCE (<parent table name>) <attribute list>;
+ADD CONSTRAINT <constraint name> FORIEGN KEY <attribute list>
+REFERENCE <parent table name> (<attribute list>);
 )
