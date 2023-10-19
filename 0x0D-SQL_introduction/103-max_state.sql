06d56f4397f1b96efab6f8e8cf2c2eb730217aa3
Yabiti <yabetseyohannes@gmail.com>
2023-10-19 19:54:13 +0300

    create

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
-CREATE TABLE <table name>
-<attribute 1> <data type 1>
-<attribute name> <data table n>
+CREATE TABLE <table name>(
+<attribute name  1> <data type 1>
+<attribute name n> <data table n>)
