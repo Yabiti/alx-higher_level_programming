c43dd12854f1e648ebdd61f61b8b699859dee44f
Yabiti <yabetseyohannes@gmail.com>
2023-10-23 19:43:36 +0300

    search

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-EnglishDictionaries = {'wusu': 'wiss', 'weey': 'wuu',}
+EnglishDictionaries = {'wusu':'wiss', 'weey':'wuu',}
 for key in sorted(EnglishDictionaries.keys()):
     print(key, "=", EnglishDictionaries.keys())
+
