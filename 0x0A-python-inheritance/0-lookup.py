902d0706685f2fa0f2fe9a6fdf4fa9006dacbfe2
Yabiti <yabetseyohannes@gmail.com>
2023-10-23 19:57:08 +0300

    conditionals

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def main():
-    strings = "i loved working with you github"
-    print(strings)
-    rawstrings = "i;m not on your level/n but we are freinds"
-    print(rawstrings)
+    conditonal_exec()
 
+def conditonal_exec():
+
+
+    
 if __name__ == "__main__":
     main()
