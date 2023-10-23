1a39b3d936d98db25c03f64ce286eafad7eed32c
Yabiti <yabetseyohannes@gmail.com>
2023-10-23 19:54:19 +0300

    github

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,8 @@
 def main():
     strings = "i loved working with you github"
     print(strings)
+    rawstrings = "i;m not on your level/n but we are freinds"
+    print(rawstrings)
 
 if __name__ == "__main__":
     main()
