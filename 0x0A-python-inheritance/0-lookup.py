5cced69aa96dd6a7e87068de65fa4be1ae580246
Yabiti <yabetseyohannes@gmail.com>
2023-10-23 20:02:31 +0300

    conditionals

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,7 +6,9 @@ def conditonal_exec():
     a,b = 1,3
     if a < b:
         print("a is less tahn b")
-
+def conditional_values():
+    a,b = 1,3
+    statements = "lessthan" a<b if lessthan else"not lessthan"
 
     
 if __name__ == "__main__":
