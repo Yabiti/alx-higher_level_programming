e2127804ee538652a769974b8d6802f85d5430ed
Yabiti <yabetseyohannes@gmail.com>
2023-10-27 18:48:35 +0300

    list

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
--- inserts new row into table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- a script that delete the database hbtn_0c_0 in your MySQL server 
+-- because Batch 3 is the best!
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School'); 
