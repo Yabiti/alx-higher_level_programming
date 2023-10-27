5be1c25f7397e459248cba141bc4515bc991df91
Yabiti <yabetseyohannes@gmail.com>
2023-10-27 20:45:02 +0300

    .

--- 0x0D-SQL_introduction/104.sql
+++ 0x0D-SQL_introduction/104.sql
@@ -1,1 +1,1 @@
-SELECT posts.title FROM posts JOIN comments ON posts_id = comments.posts_id = where comments published=1
+SELECT posts.title FROM posts JOIN comments ON posts.id = comments.posts_id = WHERE comments published=1;
