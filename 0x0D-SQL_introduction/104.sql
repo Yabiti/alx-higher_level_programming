9a8776f9f69603b854f0cbcfb1f97e099d8da04c
Yabiti <yabetseyohannes@gmail.com>
2023-10-27 20:13:04 +0300

    lists

--- 0x0D-SQL_introduction/104.sql
+++ 0x0D-SQL_introduction/104.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(id) FROM first_table WHERE id=89;
+SELEXT COUNT(id) FROM first_table WHERE id=89;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- a script that delete the database hbtn_0c_0 in your MySQL server 
 -- because Batch 3 is the best!
 INSERT INTO first_table (id, name)
-VALUES (89, 'Best School'); 
+VALUES (89, 'Best School');
