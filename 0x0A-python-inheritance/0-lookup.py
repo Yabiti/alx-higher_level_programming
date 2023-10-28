7ccee9a3f712cc016540fabb7cd8598b4902ec67
Yabiti <yabetseyohannes@gmail.com>
2023-10-28 12:23:26 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -11,4 +11,4 @@ print('socket has been created')
 try:
     mySocket.bind((HOST, PORT))
 except socket.error as msg:
-    print("Binding has failed.Error Code is:")
+    print("Binding has failed.Error Code is: 'message' + str(msg[0]) + 'mesage' + msg(1)")
