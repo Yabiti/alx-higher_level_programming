a7af9d9a6e9d9c7b3c4e78ca398ca83d86adb7fe
Yabiti <yabetseyohannes@gmail.com>
2023-10-28 12:22:33 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,3 +7,8 @@ PORT = 9000
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('socket has been created')
+
+try:
+    mySocket.bind((HOST, PORT))
+except socket.error as msg:
+    print("Binding has failed.Error Code is:")
