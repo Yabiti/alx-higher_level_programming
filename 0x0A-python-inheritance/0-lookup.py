dbd3a8d84fa8f5f017c67d96944c6c20465915c7
Yabiti <yabetseyohannes@gmail.com>
2023-10-28 11:05:35 +0300

    lists

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,8 @@ def main():
     strings = "i love using git hub"
     print(strings)
 
+    rawstrings = "i love using git /n but without any hobby it's useless"
+    print(rawstrings)
+
 if __name__ =="__main__":
     main()
