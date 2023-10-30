70a9218968d34da462096d5b3bf220d31f3846bb
Yabiti <yabetseyohannes@gmail.com>
2023-10-30 17:41:07 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,18 +3,12 @@ import sys
 
 HOST = ""
 
-PORT = 9000
+PORT = 9090
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('socket has been created')
+print('socket has benn created')
 
 try:
     mySocket.bind((HOST, PORT))
-except socket.error as msg:
-    print("Binding has failed.Error Code is: 'message' + str(msg[0]) + 'mesage' + msg(1)")
-    sys.exit()
-print("socket bin is complete. Now we can proceed to make it listen...")
-mySocket.listen(10)
-print("socket is now listening")
-while 1:
-    address = mySocket.accept(0)
+except mySocket.Error as msg:
+    print("Binding has failed. Error Code as: 'message' + str(msg[0] + 'message' + (msg[1]))")
