a6aa83a744e71791f9d46c5a8f56a9c042448303
Yabiti <yabetseyohannes@gmail.com>
2023-10-30 18:34:29 +0300

    connect

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -13,11 +13,3 @@ try:
 except socket.error as msg:
     print("Binding has failed.Error Code as: + str(msg[0] + 'message' (msg[1]))")
     sys.exit()
-print("bind has been created, now it hasprocced to listen...")
-mySocket.listen(10)
-print("socket is now listening")
-while 1:
-    address = mySocket.accept()
-    print("conected with" + address[0] + ':' + str(address[1]))
-    mySocket.close()
-
