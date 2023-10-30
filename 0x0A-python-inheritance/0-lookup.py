be4466e37871424c24a43fb35847644ad03f3879
Yabiti <yabetseyohannes@gmail.com>
2023-10-30 17:56:06 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,3 +7,8 @@ PORT = 9090
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("socket has been created")
+
+try:
+    mySocket.bind((HOST, PORT))
+except mySocket.Error as msg:
+    print
