6cd5c6f6411e1b6fc67a12ac4ed5749f89a8f544
Yabiti <yabetseyohannes@gmail.com>
2023-10-30 17:47:17 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,3 +7,7 @@ PORT = 9090
 mySocket = socket.socket(socket.AF_INEt, socket.SOCK_STREAM)
 print("socket has benn created")
 
+try:
+    mySocket.bind((HOST, PORT))
+except mySocket.Error as msg:
+    print("Binding has failed.Error as Code: + str(msg[0] + 'message' (msg[1]))")
