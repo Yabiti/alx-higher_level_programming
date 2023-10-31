356dfa3871b26000f98586bd6e8534150be8f26d
Yabiti <yabetseyohannes@gmail.com>
2023-10-31 09:22:19 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,12 +3,3 @@ class node(object):
         self.value = value
         self.left = None
         self.right = None
-
-
-class BinaryTree(object):
-    def __init__(self, root):
-        self.root = node(root)
-
-tree = BinaryTree(1)
-tree.root.left = node(2)
-tree.root.right = node(3)
