59f1ebc40806a198fa4ee71fd7ff8c97baea16ef
Yabiti <yabetseyohannes@gmail.com>
2023-10-31 10:39:18 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -19,4 +19,5 @@ tree.root.left.right = node(3)
 tree.root.left.left = node(4)
 tree.root.right = node(5)
 tree.root.right.left = node(6)
-print(tree.print_tree('preorder'))
+
+print(tree.print_tree("preorder"))
