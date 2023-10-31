663a461ba0a1fcb8653558fd581aff8461d931a1
Yabiti <yabetseyohannes@gmail.com>
2023-10-31 15:17:03 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -18,7 +18,7 @@ class BinatryTree(object):
 
     def preorder(self, start, traversal):
         if start:
-            traversal += (str(start.value, + "-"))
+            traversal += (str(start.value) + "-")
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
         return traversal
