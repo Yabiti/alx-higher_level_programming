722b683d1287d7be82fb3cf97d03f4dccef9e0ec
Yabiti <yabetseyohannes@gmail.com>
2023-11-11 12:09:37 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 def main():
+    number = get_number()
     meow(3)
 
+def get_number():
+    while True:
+        n = int(input("what's the number? "))
+        if n > 0:
+            return n
 def meow(n):
     for _ in range(n):
         print("meow")
