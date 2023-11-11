7307b10156b4cbb5abfb5a5f1f097a437d83cdd7
Yabiti <yabetseyohannes@gmail.com>
2023-11-11 10:31:43 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,14 @@
-x = int(input("what's x? "))
+def main():
+    x = int(input("what's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("odd")
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("odd")
+def is_even(n):
+    
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+main()
