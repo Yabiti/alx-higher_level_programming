5bcf93162b6b63ed272256e5df793b85b6b85329
Yabiti <yabetseyohannes@gmail.com>
2023-11-13 21:10:18 +0300

    .

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a script that prints the addition of 2 integers
+// Function to compute the product of p1 and p2
+function add (a, b) {
+    return a + b;
+  }
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
