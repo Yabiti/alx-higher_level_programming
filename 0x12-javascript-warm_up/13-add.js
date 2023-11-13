22bc09617e179ff65a4d5a8171d2e64cf1fa10c7
Yabiti <yabetseyohannes@gmail.com>
2023-11-13 21:26:27 +0300

    .

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+// Write a script that prints the addition of 2 integers
+// Function to compute addition of two numbers
+exports.add = function add (a, b) {
+    return a + b;
+  };
