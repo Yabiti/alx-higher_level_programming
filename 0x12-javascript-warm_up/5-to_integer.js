9da7e2220be90b7b4c277457137affb581097943
Yabiti <yabetseyohannes@gmail.com>
2023-11-13 20:56:10 +0300

    .

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Write a script that prints a message depending of the number of arguments passed
 if (process.argv.slice(2).length < 1) {
-    console.log('Not a number');
-  } else if (parseInt(process.argv[2])) {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  } else {
-    console.log('Not a number');
-  }
+console.log('Not a number');
+} else if (parseInt(process.argv[2])) {
+ console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+console.log('Not a number');
+}
