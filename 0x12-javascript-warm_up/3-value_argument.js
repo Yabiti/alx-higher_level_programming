32c0f026e405dea49405a4fc6fd3bd9f3fb92053
Yabiti <yabetseyohannes@gmail.com>
2023-11-13 20:43:47 +0300

    .

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a script that prints a message depending of the number of arguments passed
+if (!process.argv[2]) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }
