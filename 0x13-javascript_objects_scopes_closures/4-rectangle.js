748812e853e4e33a08961fa162163e14418dd511
Yabiti <yabetseyohannes@gmail.com>
2023-11-14 19:56:25 +0300

    .

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        // If w and h are greater than 0
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      // nstance method called print() that prints the rectangle using the character X
+      for (let i = 0; i < this.height; i++) {
+        let MyVar = '';
+        for (let j = 0; j < this.width; j++) {
+          MyVar += 'X';
+        }
+        console.log(MyVar);
+      }
+    }
+  
+    rotate () {
+      const x = this.height;
+      this.height = this.width;
+      this.width = x;
+    }
+  
+    double () {
+      this.height *= 2;
+      this.width *= 2;
+    }
+  };
