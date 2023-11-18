47eb16ade5cff1484cc61159b3998e5e39647877
Yabiti <yabetseyohannes@gmail.com>
2023-11-18 13:51:23 +0300

    .

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python script
+"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
+                         database=argv[3], charset="utf8", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
