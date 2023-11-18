fed24ce57ec707699fdefa1ad1c2ee5622e9a819
Yabiti <yabetseyohannes@gmail.com>
2023-11-18 14:03:42 +0300

    .

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+class definition of a State
+"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+
+    """
+    class definition of a City
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
