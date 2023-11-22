4541b120fd1330f6bb69a6a7549d289358b0b3e5
Yabiti <yabetseyohannes@gmail.com>
2023-11-22 15:28:40 +0300

    .

--- 0x0A-python-inheritance/javascript/counter.js
+++ 0x0A-python-inheritance/javascript/counter.js
@@ -1,10 +1,12 @@
-let counter = 0;
-            function count() {
-                counter++;
-                document.querySelector('h1').innerHTML = counter;
+if (!localStorage.getItem(`counter`)) {
+    localStorage.setItem(`counter`, 0);
+}
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
                
-            }
-                document.addEventListener('DOMContentLoaded', function() {
-                    document.querySelector('button').onclick = count;
-                });
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = count;
+    });
         
