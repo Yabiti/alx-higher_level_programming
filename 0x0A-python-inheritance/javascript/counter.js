82fd6022f76eeffb2fcb012b79104cf2b52dd8af
Yabiti <yabetseyohannes@gmail.com>
2023-11-22 15:03:53 +0300

    .

--- 0x0A-python-inheritance/javascript/counter.js
+++ 0x0A-python-inheritance/javascript/counter.js
@@ -2,10 +2,9 @@ let counter = 0;
             function count() {
                 counter++;
                 document.querySelector('h1').innerHTML = counter;
-                if (counter % 10 === 0){
-                    alert(`count is now ${counter}`);
-                }
+               
             }
                 document.addEventListener('DOMContentLoaded', function() {
                     document.querySelector('button').onclick = count;
+                    setInterval(count, 15);
                 });
