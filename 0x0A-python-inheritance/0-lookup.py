98fddb86a1d2d5b6e20755243c82ad35c57d11ce
Yabiti <yabetseyohannes@gmail.com>
2023-11-24 19:01:32 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,4 +3,10 @@ class Flight():
         self.capacity = capacity
         self.passsengers = []
     def add_passengers(self, name):
+        if not self.open_seats():
+            return False
         self.passengers.append(name)
+        return True
+    def open_seata(self):
+        return self.capacity - len(self.passsengers)
+        
