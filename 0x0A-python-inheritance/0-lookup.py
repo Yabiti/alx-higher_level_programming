c7b955ddba45b90d671de564203eca964f5a4ffe
Yabiti <yabetseyohannes@gmail.com>
2023-11-28 13:53:22 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,12 @@
-names = ["harry","hermione", "draco"]
-print(names)
-names.append("draco")
-names.sort()
-print(names)
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(3)
+s.add(4)
+s.add(4)
+
+s.remove(2)
+
+print(f"the set has{len(s)} elements.")
