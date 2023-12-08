94983700c801fd7099bc96110bb0559de933c255
Yabiti <yabetseyohannes@gmail.com>
2023-12-08 14:07:07 +0300

    data

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+
 """
-python script
+File: 1-filter_states.py
+Desc: This module contains a script that  lists all states
+with a name starting with N (upper N) from the database hbtn_0e_0_usa
+
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Oct 7, 2022
 """
-import MySQLdb
+
 from sys import argv
+import MySQLdb
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
-                         database=argv[3], charset="utf8", port=3306)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    cur.close()
-    db.close()
+    cur.execute("""SELECT * FROM states
+            WHERE BINARY name LIKE 'N%'
+            ORDER BY id ASC""")
+    states_info = cur.fetchall()
+
+    for state in states_info:
+        print(state)
