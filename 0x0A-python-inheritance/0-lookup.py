b6cd08d82a70137a9492ae7e7362570883e63553
Yabiti <yabetseyohannes@gmail.com>
2023-12-20 13:13:40 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,19 +4,17 @@ n2 = 7
 q = "what is " + str(n1)  + "+" + str(n2) + "?" + " ans: " 
 user_answer = input(q)
 print("your answer is", user_answer)
-print("the correct answer is", n1 + n2)
+# Initialize a variable 'full_name' with an empty string
+full_name = ""
 
-q = "what is " + str(n1) + "-" + str(n2) + "?" + " ans: "
-user_answer= input(q)
-print("your anser is", user_answer)
-print("the correct answer is ", n1 - n2)
+# Prompt the user to enter their first name and store it in the 'first_name' variable
+first_name = input("Enter your first name: ")
 
-q = "what is" + str(n1) + "*" + str(n2) + "?" + " ans:"
-user_answer = input(q)
-print("your answer is ", user_answer)
-print("the correct answer is ", n1 * n2)
+# Prompt the user to enter their last name and store it in the 'last_name' variable
+last_name = input("Enter your last name: ")
 
-q = "what is " + str(n1) + "/" + str(n2) + "?" " ans:"
-user_answer = input(q)
-print("your answer is", user_answer)
-print("the correct answer is ", n1 / n2)
+# Concatenate the names with a space in between and store the result in 'my_name'
+full_name = first_name + " " + last_name # The `+` symbol can be used to combine two strings.
+
+# Display a message with the user's full name using the 'full_name' variable
+print("My name is", full_name)
