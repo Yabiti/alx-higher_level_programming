2824288a0ba231601875d3b4aaac6d7464487b46
Yabiti <yabetseyohannes@gmail.com>
2023-12-20 12:32:53 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 def order_car(car_type, manufacturing_year):
     car = "this car has", car_type , "and", manufacturing_year
     return car_type
+first_car = order_car("toyota", 2018)
+print(first_car)
+
+second_car = order_car("buggati", 2023)
+print(second_car)
