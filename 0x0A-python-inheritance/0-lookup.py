1cf3df2a8dac00996985c689d23d7238472beee1
Yabiti <yabetseyohannes@gmail.com>
2023-12-20 13:35:02 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,20 +1,5 @@
-n1 = 5
-n2 = 7
+def calculate(n1, n2 ,n3):
+    total = n1 + n2 + n3
+    print(calculate(3,7,5))
 
-q = "what is " + str(n1)  + "+" + str(n2) + "?" + " ans: " 
-user_answer = input(q)
-print("your answer is", user_answer)
-# Initialize a variable 'full_name' with an empty string
-full_name = ""
-
-# Prompt the user to enter their first name and store it in the 'first_name' variable
-first_name = input("Enter your first name: ")
-
-# Prompt the user to enter their last name and store it in the 'last_name' variable
-last_name = input("Enter your last name: ")
-
-# Concatenate the names with a space in between and store the result in 'my_name'
-full_name = first_name + " " + last_name # The `+` symbol can be used to combine two strings.
-
-# Display a message with the user's full name using the 'full_name' variable
-print("My name is", full_name)
+    print(calculate(3,5))
