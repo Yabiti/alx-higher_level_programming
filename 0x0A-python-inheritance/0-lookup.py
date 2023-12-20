eef8eca6fb61850a2fa763b2d4ddcb0608800201
Yabiti <yabetseyohannes@gmail.com>
2023-12-20 10:54:22 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,4 @@
 num1 = int(input("first number "))
 num2 = int(input("Second number "))
 
-print("the answer to", num1, "-", num2, "is", num1 - num2)
+print("the answer to", num1, "/", num2, "is", num1 / num2)
