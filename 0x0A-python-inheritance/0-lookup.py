09a222397c857e156a9bb84073fd6be0b8ce67c5
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 11:04:22 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,3 +2,4 @@ user_answer = int(input("what's 5 * 6? "))
 correct_answer = 5 * 6
 print("user_answer", user_answer, "correct_answer", correct_answer)
 user_answer == correct_answer
+print("user_answer", user_answer, "correct_answer", correct_answer)
