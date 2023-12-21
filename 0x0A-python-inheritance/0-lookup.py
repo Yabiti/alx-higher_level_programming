e7956071a156eaa38eba35f75f1130f9d500d186
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 11:25:49 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,3 @@
-score = 0
-print("your score is", score)
-user_answer = int(input("what's 5 * 6? "))
-correct_answer = 5 * 6
-if user_answer == correct_answer:
-    score = score + 1
-    print("great your new score is:", score)
-else:
-    print("badly it's a wrong answer")
+hour = 15
+if hour < 12:
+    greeting = "Good morning"
