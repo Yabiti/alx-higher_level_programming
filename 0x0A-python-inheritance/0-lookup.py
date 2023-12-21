078f4a4f832127583278487b1a0951d5269087b2
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 11:23:10 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,6 @@ user_answer = int(input("what's 5 * 6? "))
 correct_answer = 5 * 6
 if user_answer == correct_answer:
     score = score + 1
-    print("your score is:", score)
+    print("great your new score is:", score)
 else:
     print("badly it's a wrong answer")
