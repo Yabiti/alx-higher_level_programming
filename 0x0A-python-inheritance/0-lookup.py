5c3a30f5f8a7b11e5cce2a85eaba58434842d680
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 16:35:03 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,2 @@
-name = input("what's your name? ")
-print(f"welcome, {name}, please take this test!")
-q = "what's 6 + 2 "
-user_answer = input(q)
-print(user_answer)
-farewell_message = "hello", name, "thanks for taking the test"
-print(farewell_message)
+n1 = 7
+n2 = 12
