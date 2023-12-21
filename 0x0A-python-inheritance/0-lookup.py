8f4f556ef8cf2509aed964048c6e788fc595548f
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 11:22:25 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 score = 0
 print("your score is", score)
 user_answer = int(input("what's 5 * 6? "))
-user_answer = int(input("what's 5 * 7? "))
 correct_answer = 5 * 6
-correct_answer = 5 * 7
 if user_answer == correct_answer:
     score = score + 1
-    score = score + 1
-print("your score is:", score)
+    print("your score is:", score)
+else:
+    print("badly it's a wrong answer")
