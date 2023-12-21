cbed8d8e1e4abfc3d6f0a653bfa24023d65b74bc
Yabiti <yabetseyohannes@gmail.com>
2023-12-21 11:02:56 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,1 @@
-user_answer = int(input("what's 4 * 7? "))
-correct_answer = 4 * 7
-print("user_answer", user_answer, "correct_answer", correct_answer)
-user_answer == correct_answer
-print("user_answer", user_answer, "correct_answer", correct_answer)
+user_answer = int(input("what's 5 * 6? "))
