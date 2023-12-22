e6351a9285817e67bd31a77715c29c9816e103e0
Yabiti <yabetseyohannes@gmail.com>
2023-12-22 11:23:48 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 def order_metn(berbere, shro, mitmita):
     metn = "make a " + berbere + "."
-
+    metn = "make a " + shro + "."
+    metn = "make a" + mitmita + "."
     return metn
 first_order = order_metn("berbere")
 print(first_order)
