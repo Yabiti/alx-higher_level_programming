b1cf8243f18936acd81a7f09de6364ee610e546d
Yabiti <yabetseyohannes@gmail.com>
2023-12-22 11:17:52 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,3 +4,4 @@ def order_pizza(pizza_type, Extra_topping):
 first_pizza = order_pizza("pepperoni", "lice")
 print(first_pizza)
 second_pizza = order_pizza("pepperoni", "slucce")
+print(second_pizza)
