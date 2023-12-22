d64d4c26932520be92837cd7287f26860df107ed
Yabiti <yabetseyohannes@gmail.com>
2023-12-22 19:59:57 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,4 @@
+score = 0
 name = input("what's your name? ")
 print("hello", name, "welcome to maths test")
 
@@ -7,3 +8,4 @@ correct_answer = 6 + 7
 print("correct_answer", correct_answer)
 if correct_answer == user_answer:
     score = score + 1
+    print("score", score)
