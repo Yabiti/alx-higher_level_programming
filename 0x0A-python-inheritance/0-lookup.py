4d4dffc515041abfdd707775c9367c8f864a7dee
Yabiti <yabetseyohannes@gmail.com>
2023-12-22 19:57:32 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 name = input("what's your name? ")
 print("hello", name, "welcome to maths test")
 
-user_answer = input("what's 6 + 7")
+user_answer = input("what's 6 + 7 ")
 print("user_answer", user_answer)
 correct_answer = 6 + 7
 print("correct_answer", correct_answer)
