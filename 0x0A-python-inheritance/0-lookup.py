3267534e628133fd69e3b017c51002b897bd6be0
Yabiti <yabetseyohannes@gmail.com>
2023-12-23 09:51:26 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -20,4 +20,10 @@ user_ans = input("Ans ")
 correct_ans = n1 - n2
 answer_feedback(user_ans, correct_ans)
 
+print_question(n1, "*", n2)
+user_ans = input("Ans ")
+correct_ans = n1 * n2
+answer_feedback(user_ans, correct_ans)
+
+
 
