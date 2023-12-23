f43431cd510ff04baca637f5831b5eb8dfe263f3
Yabiti <yabetseyohannes@gmail.com>
2023-12-23 09:46:50 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,3 +2,5 @@ def print_question(num1 , op , num2):
     print()
     q = "what's is " + str(num1) + op + str(num2) + "? "
     print(q)
+def answer_feedback(user_ans. correct_ans):
+    print("user_answer: ", user_ans)
