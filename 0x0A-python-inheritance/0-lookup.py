a756dd6fbbf6a55b59482df1ae3e04693d5d7a97
Yabiti <yabetseyohannes@gmail.com>
2023-12-23 09:24:33 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 n1 = 4
 n2 = 7
 
-q = "what is " + str(n1) + "+" (str(n2)) + "?"
+q = "what is " + str(n1) + "+" + str(n2) + "?"
 user_answer = input(q)
+print("user_answer", user_answer)
+correct_answer = n1 + n2
