3977110477198997de31c662cb57c28beede192a
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 07:52:18 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,31 +1,4 @@
-def print_question(num1 , op , num2):
-    print()
-    q = "what's is " + str(num1) + op + str(num2) + "? "
-    print(q)
+name = input("what's your name? ")
 
-def answer_feedback(user_ans, correct_ans):
-    print("user_answer: ", user_ans)
-    print("correct_answer", correct_ans)
-
-n1 = 5
-n2 = 7
-
-print_question(n1, "+", n2)
-user_ans = input("Ans ")
-correct_ans = n1 + n2
-answer_feedback(user_ans, correct_ans)
-
-print_question(n1, "-", n2)
-user_ans = input("Ans ")
-correct_ans = n1 - n2
-answer_feedback(user_ans, correct_ans)
-
-print_question(n1, "*", n2)
-user_ans = input("Ans ")
-correct_ans = n1 * n2
-answer_feedback(user_ans, correct_ans)
-
-print_question(n1, "/", n2)
-user_ans = input("Ans ")
-correct_ans = int(n1 / n2)
-answer_feedback(user_ans, correct_ans)
+welcome_message = "hello " + name + "to math test Good Luck"
+print(welcome_message)
