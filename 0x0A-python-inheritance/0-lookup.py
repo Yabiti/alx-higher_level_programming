becc3ec9df335b3086674581622b7ffb6efb441e
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 08:26:51 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,5 +2,5 @@ import random
 
 name = input("what's your name? ")
 print(name)
-name = "Hello " + name + " welcome to math test Good luck!"
+name = "Hello " + name + " welcome to math test. Good luck!"
 print(name)
