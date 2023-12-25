3197277eb8e53add1ecc1c654b175b3b3fdb2a00
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 08:21:53 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,3 @@
 import random
-name = input("what's your name? ")
 
-welcome_message = "hello " + name + " welcome to math test Good Luck!"
-print(welcome_message)
-
-num1 = random.randint(1, 5)
-num2 = random.randint(1, 5)
-op = "+"
-
-q = "what is " + str(num1) + op + str(num2) + "? "
-print(q)
+print(random.randint(1,10))
