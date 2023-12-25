28a2f1cc3c6dfddc4b27e3f2096143d867348126
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 11:09:47 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,3 +5,7 @@ def greet_user(user_name):
 def print_qusetion(num1, num2 ,op):
     q = "what is " + str(num1) + op + str(num2) + "? "
     print(q)
+
+def print_result(user_name, score):
+    farewell_message = farewell_message = "thanks " + user_name + "your score is" + str(score) + "out of 5"
+    print(farewell_message)
