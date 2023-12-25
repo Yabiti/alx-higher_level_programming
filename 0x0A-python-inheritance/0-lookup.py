85782c1ce606a35452b9bff208808ae7f021b97b
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 11:26:15 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -45,6 +45,5 @@ total = 0
 while total < 5:
     num1 = random.randint(1, 10)
     num2 = random.randint(1,10)
-
-    op = choose_opretator(op_num)
     op_num = random.randint(1,4)
+    op = choose_opretator(op_num)
