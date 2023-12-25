caf12c49ad6ea0c90b7351b2ce2a7e21941a41f7
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 09:26:00 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 def greet_user(user_name):
     welcome_message = "hello" + user_name + "welcome to math test Good Luck!"
     print(welcome_message)
+
+def choose_operator(op_n):
+    if op_n == 1:
+        return "-"
+    elif op_n == 2:
+        return "+"
+    elif op_n == 3:
+        return "*"
+    elif op_n == 4:
+        return "/"
