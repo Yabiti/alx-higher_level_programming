2d26fa3ef5ef2aa5bc16ce563af7c176233cf4d9
Yabiti <yabetseyohannes@gmail.com>
2023-12-25 11:06:54 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 def greet_user(user_name):
     welcome_messsage = "Hello " + user_name + "welcome to math test"
     print(welcome_messsage)
+
+def print_qusetion(num1, num2 ,op):
+    q = "what is " + str(num1) + op + str(num2) + "? "
+    print(q)
