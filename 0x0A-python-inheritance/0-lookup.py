62cf5439d0a916d044951e5f1fefb4eb0f09c24d
Yabiti <yabetseyohannes@gmail.com>
2023-12-26 17:07:34 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -21,9 +21,11 @@ def choose_operator(op_n):
 def calculate_question(n1, n2 , op):
     if op == "+":
         return n1 + n2
-    if op == "-":
+    elif op == "-":
         return n1 - n2
-    if op == "*":
+    elif op == "*":
         return n1 * n2
-    if op == "/":
+    elif op == "/":
         return n1 / n2
+    else:
+        return "invalid operator"
