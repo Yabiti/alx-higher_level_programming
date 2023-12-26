02b67e3be5e3532b5a48d0e2341398d7faa69e79
Yabiti <yabetseyohannes@gmail.com>
2023-12-26 16:54:34 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,4 @@
 user_name = input("what's your name? ")
 
-welcome_message = "Hello" + user_name + "welcome to math test"
+welcome_message = "Hello " + user_name + " welcome to math test"
 print(welcome_message)
