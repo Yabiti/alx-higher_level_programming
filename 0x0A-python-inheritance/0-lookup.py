0ba4dfcef80ecfa440da55d824782a9680fd9e5f
Yabiti <yabetseyohannes@gmail.com>
2023-12-26 17:00:54 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,3 +5,10 @@ def greet_user(user_name):
 def print_question(num1, num2, op):
     q = "what is " + str(num1) + op + str(num2) + "? "
     print(q)
+
+def calculate_question(op_num):
+    if op_num == 1:
+        return "+"
+    if op_num == 2:
+        return "-"
+    
