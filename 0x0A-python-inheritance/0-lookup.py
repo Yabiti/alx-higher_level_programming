cbe8f7b00d5d531bf7e9aff0648aa69a1d5eef80
Yabiti <yabetseyohannes@gmail.com>
2023-12-27 10:02:21 +0300

    /

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 score = 0
-print("your naswer is: ", score)
-user_answer = input("what's 4 * 7")
+print("your score is: ", score)
+user_answer = input("what's 4 * 7? ")
+correct_answer = 4 * 7
+if user_answer == correct_answer:
+    score = score + 1
+print("Your new score is", score)
